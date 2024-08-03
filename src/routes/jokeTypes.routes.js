7f4b869d1b854b743bb9bea150b9c800e5de02f5
kumuditha-udayanga <kumudithaudaya@gmail.com>
2024-08-03 17:25:35 +0530

    Define routes for jokes and jokeTypes

---
+++ src/routes/jokeTypes.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { createJokeType, getJokeTypes } from "../controllers/jokeTypes.controller.js";
+
+export const jokeTypeRoutes = express.Router();
+
+jokeTypeRoutes.route("/").post(createJokeType);
+jokeTypeRoutes.route("/").get(getJokeTypes);
---
+++ src/routes/jokes.routes.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { createJoke, getRandomJokeFromType } from "../controllers/jokes.controller.js";
+
+export const jokeRoutes = express.Router();
+
+jokeRoutes.route("/").post(createJoke);
+jokeRoutes.route("/").get(getRandomJokeFromType);
