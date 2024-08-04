078008e5debd08724d41caad9d68f472731d8060
kumuditha-udayanga <kumudithaudaya@gmail.com>
2024-08-04 11:55:08 +0530

    Update credientials

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2";
 import { config } from "../../config.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Create database connection
 export const pool = mysql.createPool({
-    host: "localhost",
-    database: "jokes_db",
-    user: "root",
+    host: config.HOST,
+    database: config.DB,
+    user: config.USER,
     password: "root"
 }).promise();
 
